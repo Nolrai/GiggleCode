e8025350bf5cdd792e32c31fcd040cd04ac7b675
Chris A. Upshaw <chrisaupshaw@gmail.com>
2015-12-01 00:19:32 -0800

    Master should always compile.
    Revert "First night of work. Doesn't compile yet."
    
    This reverts commit 9541ca53ad2ffc9d780fa8900e30ca1ee105d646.

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,12 +1,6 @@
 module Lib
-    ( compress
-    , decompress
+    ( someFunc
     ) where
-import qualified Data.ByteString at B
-import qualified Data.Text as T
 
-compress :: T.Text -> B.ByteString
-compress = padicEncode . grammarToList . buildGrammar
-
-decompress :: B.ByteString -> T.Text
-decompress = inflateGrammar . listToGrammar . decodePadic
+someFunc :: IO ()
+someFunc = putStrLn "someFunc"
