705707f75d51ecdb20c3fa2879237d66e986027d
Christopher A. Upshaw <chrisaupshaw@gmail.com>
2016-05-12 11:18:55 -0700

    first stab at TestUtils after renaming from Helpers

--- test/TestUtils.hs
+++ test/TestUtils.hs
@@ -1,11 +1,11 @@
-module Helpers where
+module TestUtils where
 
 import Test.Framework (defaultMain, testGroup)
 import Test.Framework.Providers.QuickCheck2 (testProperty)
 
 import Test.QuickCheck
 
-areInverses (fname, f) (gname, g) = 
+areInverses (fname, f) (gname, g) =
 	testGroup
 	(fname ++ " " ++ gname ++ " are inverses.")
 	[ (fname, f) `isInverseOf` (gname, g)
