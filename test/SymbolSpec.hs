b1bb20136b443c340c186231a827a40bc00342f9
Chris Upshaw <chrisaupshaw@gmail.com>
2016-11-12 08:26:35 -0800

    still working on this.

--- test/SymbolSpec.hs
+++ test/SymbolSpec.hs
@@ -1,20 +1,45 @@
-{-# LANGUAGE StandaloneDeriving, GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE StandaloneDeriving, DeriveGeneric #-}
 module SymbolSpec where
 import Symbol
 
 import TestUtils
-import Test.QuickCheck (property, Arbitrary, (==>))
+import Test.QuickCheck (property, Arbitrary(..), (==>))
 import GrammarSpec ()
-import Grammar ()
+import Grammar (Node)
+import qualified Data.Vector as V
+import Data.Vector (Vector)
+import Control.Exception (throw)
+import Control.Applicative ((<$>),(<*>))
 
 deriving instance Arbitrary Symbol
 
-isValue :: Eq a => a -> Bool
-isValue x = if x == x then True else (error "isValue: x == x is false.")
+doesThrow = isRight . tryEvaluate . force
 
 spec :: Spec
 spec =
   do
-  isInverseOf ("unsafeToNode", unsafeToNode) ("toSymbol", toSymbol)
-  it "isNode allows unsafeToNode" $ property (\ x  -> isNode x ==> isValue (unsafeToNode x) )
-  areInverses ("breakAtEndline", breakAtEndline) ("unBreakAtEndline", unBreakAtEndline)
+  isInverseOf
+    ("unsafeToNode", unsafeToNode)
+    ("toSymbol", toSymbol)
+  it "isNode allows unsafeToNode"
+    $ property (\ x  -> isNode x == (doesThrow $ unsafetoNode x))
+  areInverses
+    ("breakAtEndline", breakAtEndline')
+    ("unBreakAtEndline", unBreakAtEndline')
+  where
+  breakAtEndline' :: Valid -> (Vector Node, Vector Symbol)
+  breakAtEndline' = breakAtEndline . fromValid
+  unBreakAtEndline' :: (Vector Node, Vector Symbol) -> Valid a
+  unBreakAtEndline' = unsafeMkValid . unBreakAtEndline
+
+instance Arbitrary Valid where
+  arbitrary = Valid <$> arbitrary <*> arbitrary
+  shrink = genericShrink
+
+data Valid = Valid (Vector Node) (Vector Symbol)
+  deriving (Show, Read, Eq, Generic)
+
+unsafeMkValid vec e =
+  if any (== endline) vec
+    then Valid vec
+    else throw e
