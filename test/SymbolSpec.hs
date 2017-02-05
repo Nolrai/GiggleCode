30aed57865a9763c546f9580cc4a5f10bf2a5ef4
Chris Upshaw <chrisaupshaw@gmail.com>
2017-02-05 14:48:44 -0800

    work in progress

--- test/SymbolSpec.hs
+++ test/SymbolSpec.hs
@@ -1,19 +1,25 @@
-{-# LANGUAGE StandaloneDeriving, DeriveGeneric #-}
+{-# LANGUAGE StandaloneDeriving, DeriveGeneric, GeneralizedNewtypeDeriving #-}
 module SymbolSpec where
 import Symbol
 
 import TestUtils
-import Test.QuickCheck (property, Arbitrary(..), (==>))
+import Test.QuickCheck (property, Property, Arbitrary(..), (==>), shrink, genericShrink)
+import Test.QuickCheck.Exception (tryEvaluate, discard)
+import Test.QuickCheck.Monadic (monadicIO, pre, run)
+import qualified Test.QuickCheck.Monadic as M
 import GrammarSpec ()
 import Grammar (Node)
 import qualified Data.Vector as V
 import Data.Vector (Vector)
-import Control.Exception (throw)
+import qualified Control.Exception as E
 import Control.Applicative ((<$>),(<*>))
+import GHC.Generics
+import Control.DeepSeq (force)
 
 deriving instance Arbitrary Symbol
 
-doesThrow = isRight . tryEvaluate . force
+isLeft (Left _) = True
+isLeft (Right _) = False
 
 spec :: Spec
 spec =
@@ -22,24 +28,33 @@ spec =
     ("unsafeToNode", unsafeToNode)
     ("toSymbol", toSymbol)
   it "isNode allows unsafeToNode"
-    $ property (\ x  -> isNode x == (doesThrow $ unsafetoNode x))
+    $ property testIsNodeUnsafeToNode
   areInverses
     ("breakAtEndline", breakAtEndline')
     ("unBreakAtEndline", unBreakAtEndline')
   where
   breakAtEndline' :: Valid -> (Vector Node, Vector Symbol)
   breakAtEndline' = breakAtEndline . fromValid
-  unBreakAtEndline' :: (Vector Node, Vector Symbol) -> Valid a
+  unBreakAtEndline' :: (Vector Node, Vector Symbol) -> Valid
   unBreakAtEndline' = unsafeMkValid . unBreakAtEndline
+  testIsNodeUnsafeToNode :: Symbol -> Property
+  testIsNodeUnsafeToNode x = monadicIO
+    $ do
+      (pre . isNode) x
+      r <- (run . tryExecute . unsafeToNode) x
+      (M.assert . isLeft) r
 
 instance Arbitrary Valid where
-  arbitrary = Valid <$> arbitrary <*> arbitrary
-  shrink = genericShrink
+  arbitrary = discardInvalid <$> arbitrary
+  shrink v = map discardInvalid . shrink
 
-data Valid = Valid (Vector Node) (Vector Symbol)
-  deriving (Show, Read, Eq, Generic)
+discardInvalid v =
+  if all (/= endline) v
+    then discard
+    else Valid v
 
-unsafeMkValid vec e =
-  if any (== endline) vec
-    then Valid vec
-    else throw e
+data Valid = Valid {fromValid :: Vector Symbol}
+  deriving (Show, Eq, Generic)
+
+unsafeMkValid vec =
+  E.assert (any (== endline) vec) $ Valid vec
