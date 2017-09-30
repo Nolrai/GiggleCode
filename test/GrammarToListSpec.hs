787b06d499c4dda19ec7a81cd6e1f6d3d781b8ec
Christopher A. Upshaw <chrisaupshaw@gmail.com>
2017-09-30 03:56:10 -0700

    Passes Tests!

--- test/GrammarToListSpec.hs
+++ test/GrammarToListSpec.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE FlexibleInstances, FlexibleContexts, NamedFieldPuns, GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE FlexibleInstances, FlexibleContexts, NamedFieldPuns #-}
 module GrammarToListSpec
   ( spec
   ) where
@@ -8,28 +8,35 @@ import GrammarToList
   , listToGrammar
   )
 
-import Grammar (Grammar, Node(..))
-import Symbol (UnsafeToNodeEndline, TailVEmptyVector, endline, toSymbol)
-
+import Grammar (Grammar(..), Node(..))
+import Symbol
+  (UnsafeToNodeEndline, TailVEmptyVector, endline, toSymbol)
 import TestUtils
 import SymbolSpec (Ends, toValid, fromValid)
-
 import Control.Monad.Exception
+import Data.Vector (fromList)
 
 gltest :: Monad m => Grammar -> m Ends
 lgtest 
   :: ( Throws UnsafeToNodeEndline l
      , Throws TailVEmptyVector l
      ) => Ends -> EM l Grammar
-gltest = (fromValid <$>) . grammarToList
+gltest g = fromValid <$> grammarToList g
 lgtest = listToGrammar . toValid
 
 spec :: Spec
-spec =
-  describe "debug" 
-  	$ gltest (Grammar [NonTermNode 0] []) 
-	`shouldBe`
-	fromValid [toSymbol (NonTermNode 0), endline]
-  areInverses
-    ("grammarToList", gltest)
-    ("listToGrammar", lgtest)
+spec = part1 >> part2
+  where
+  part1 :: Spec
+  part1 =
+    it "takes an empty grammar to an almost empty vector" . example 
+    $ do 
+      result <- gltest (Grammar (fromList [NonTermNode 0]) mempty)
+      let expected = fromValid (fromList [toSymbol (NonTermNode 0), endline])
+      result `shouldBe` expected
+      
+  part2 :: Spec
+  part2 =
+    areInverses
+      ("grammarToList", gltest)
+      ("listToGrammar", lgtest)
