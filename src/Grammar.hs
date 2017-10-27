059fd477e155906e8698ce6d9f827b7567fc79ca
Chris Upshaw (From Yukari) <chrisaupshaw@gmail.com>
2017-10-27 00:58:17 -0700

    WIP

--- src/Grammar.hs
+++ src/Grammar.hs
@@ -1,19 +1,42 @@
 module Grammar
     ( Grammar (..)
-    , Line
-    , NonTerm
+    , Word5
     , Node (..)
-    , Term
+    , toNode
+    , fromNode
+    , isTerm
+    , isNonTerm
     ) where
-import Data.Vector (Vector)
+import Data.Text.Lazy as T
+import Data.ShortWord (Word5)
 
-type Term = Char
-type NonTerm = Int
+-- "In addition, there is a contiguous range of another 32 noncharacter code 
+--   points in the BMP: U+FDD0..U+FDEF." 
+--   - from section 23.7 of the Unicode Standard 
+-- We use these to code NonTerm symbols in the built grammar, 
+--   there are another 32 noncharacter codes, but they are non contiguous.
+nonTermStart = '\xFDD0'
+nonTermEnd = '\xFDEF'
 
-data Node = TermNode Term | NonTermNode NonTerm
-  deriving (Read, Show, Eq, Ord)
+toNode :: Char -> Node
+toNode t = 
+  if isNonTerm $ t
+    then NonTerm (toEnum . fromEnum $ t - nonTermStart) 
+    else Term t
 
-type Line = Vector Node
+fromNode :: Node -> Char
+fromNode (Term t) = t
+fromNode (NonTerm x) = chr $ ord nonTermStart + fromEnum x
 
-data Grammar = Grammar Line (Vector Line)
+asNode :: (Node -> Node) -> Char -> Char
+asNode f = toNode . f . fromNode
+
+isTerm :: Char -> Node
+isNonTerm :: Char -> Node
+isTerm t = t < nonTermStart || t > nonTermEnd
+isNonTerm t = t >= nonTermStart && t <= nonTermEnd
+
+data Node = Term Char | NonTerm Word5
+
+data Grammar = Grammar Text (Vector Text)
   deriving (Read, Show, Eq, Ord)
